c16c6a45fd1257445ef2c5a466a0bca5c1feba5e
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2026-04-26 21:27:10 -0700

    Fix location of @echo off
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- package.bat
+++ package.bat
@@ -1,3 +1,5 @@
+@echo off
+
 rem
 rem Copyright (c) Contributors to the Open 3D Engine Project. For complete copyright and license terms please see the LICENSE at the root of this distribution.
 rem
@@ -5,7 +7,6 @@ rem SPDX-License-Identifier: Apache-2.0 OR MIT
 rem
 
 
-@echo off
 rem Launch build_package.py in this directory with all forwarded arguments
 setlocal
 set "SCRIPT=%~dp0\Scripts\packaging\package.py"
